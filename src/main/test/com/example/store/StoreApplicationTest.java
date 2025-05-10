330a6f8eed3a9d3887bc8e72acf52fd88d11d5e5
Valentin <vgeadau@yahoo.com>
2025-05-10 11:07:16 +0300

    Added Spring context test (or application context smoke test).

---
+++ src/main/test/com/example/store/StoreApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StoreApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
