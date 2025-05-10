30861abba1f5203a4cbac263546e093f139d1303
Valentin <vgeadau@yahoo.com>
2025-05-11 00:52:05 +0300

    Fixed unit tests location.

--- src/test/java/com/example/store/config/WebConfigTest.java
+++ src/test/java/com/example/store/config/WebConfigTest.java
@@ -1,5 +1,6 @@
 package com.example.store.config;
 
+import com.example.store.config.WebConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
