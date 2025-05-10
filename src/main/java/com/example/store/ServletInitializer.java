b24e016012f2fc0eb77a405c01b6f859362c637c
Valentin <vgeadau@yahoo.com>
2025-05-10 11:12:22 +0300

    Fixed warnings. Added JAVADOC comments.

--- src/main/java/com/example/store/ServletInitializer.java
+++ src/main/java/com/example/store/ServletInitializer.java
@@ -3,8 +3,17 @@ package com.example.store;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * ServletInitializer class.
+ */
+@SuppressWarnings("unused")
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	/**
+	 * configures the resources.
+	 * @param application SpringApplicationBuilder
+	 * @return SpringApplicationBuilder object
+	 */
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(StoreApplication.class);
--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -3,9 +3,16 @@ package com.example.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application. This is the runnable class.
+ */
 @SpringBootApplication
 public class StoreApplication {
 
+	/**
+	 * main method.
+	 * @param args String array
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
 	}
