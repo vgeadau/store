1d47268433ae7218c8ec5564fe5a04775c6274bf
Valentin <vgeadau@yahoo.com>
2025-05-10 22:27:58 +0300

    Code review fix. Added class information.

--- src/main/test/com/example/store/security/JwtUtilTest.java
+++ src/main/test/com/example/store/security/JwtUtilTest.java
@@ -12,6 +12,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+/**
+ * Unit tests for {@link JwtUtil}.
+ */
 @ExtendWith(MockitoExtension.class)
 class JwtUtilTest {
 
