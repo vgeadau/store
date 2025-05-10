e39435cadd35d3e1672f7959cd993249a4c99251
Valentin <vgeadau@yahoo.com>
2025-05-11 00:57:40 +0300

    removed unused import.

--- src/test/java/com/example/store/config/WebConfigTest.java
+++ src/test/java/com/example/store/config/WebConfigTest.java
@@ -1,6 +1,5 @@
 package com.example.store.config;
 
-import com.example.store.config.WebConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
