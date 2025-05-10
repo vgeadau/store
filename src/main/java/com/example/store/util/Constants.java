4705f09e1a982de0f7fa22cb84afa7c808e12315
Valentin <vgeadau@yahoo.com>
2025-05-10 21:06:58 +0300

    Added error messages and constants.

---
+++ src/main/java/com/example/store/util/Constants.java
@@ -0,0 +1,18 @@
+package com.example.store.util;
+
+/**
+ * Class used to keep Store specific constants.
+ */
+public final class Constants {
+    /**
+     * Constants related to {@link com.example.store.config.GlobalExceptionHandler}.
+     */
+    public final static String ERROR = "error";
+
+    /**
+     * private constructor specific to utility classes.
+     */
+    private Constants() {
+        throw new UnsupportedOperationException("You are not allowed to extend or instantiate this utility class");
+    }
+}
--- src/main/java/com/example/store/util/ErrorMessages.java
+++ src/main/java/com/example/store/util/ErrorMessages.java
@@ -13,6 +13,22 @@ public final class ErrorMessages {
     }
 
     /**
+     * {@link com.example.store.config.GlobalExceptionHandler} error messages.
+     */
+    public final static String STORE_EXCEPTION_LOG_MESSAGE = "Store exception occurred: ";
+    public final static String GENERIC_EXCEPTION_LOG_MESSAGE = "Handled generic exception: ";
+    public final static String GENERIC_EXCEPTION_RESPONSE_MESSAGE
+            = "An unexpected error occurred. Please, contact support!";
+
+    /**
+     * {@link com.example.store.service.ProductService} error messages.
+     */
+    public static final String PRODUCT_NOT_FOUND = "Product not found!";
+    public static final String INVALID_USER = "Invalid user";
+    public static final String REMOVE_NOT_ALLOWED = "You are not allowed to remove this product";
+    public static final String PRODUCT_HAS_INVALID_USER = "Attempting to persist a product that doesn't have an existing author!";
+
+    /**
      * {@link com.example.store.service.AuthService} error messages.
      */
     public static final String AUTHENTICATION_ERROR = "Invalid username or password";
